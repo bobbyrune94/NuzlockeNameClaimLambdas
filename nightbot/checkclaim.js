ce09688b053068f7741222acf707defd03991ffb
Kenneth Chen <kc5by@virginia.edu>
2022-07-10 01:07:19 -0400

    removed comments from nightbot command

--- nightbot/checkclaim.js
+++ nightbot/checkclaim.js
@@ -1,12 +1,7 @@
-/*
-* INPUT <string> response --- the response to the ClaimTablesGetPokemonClaimLambda
-* OUTPUT <string> --- the nickname associated with the pokemon, or error message if there was one
-*/
-
 if (response['statusCode'] == 500 ) {
     `Error Getting Data. DM @Kungfu_Kenny98 to see what the error was`
 }
-if (response['statusCode'] == 404 ) {
+else if (response['statusCode'] == 404 ) {
     `Invalid Pokemon. Please check your spelling. For some special names, check here: https://www.pokeclaim.com/about#About-details`
 }
 else if (response['body']['discord-id'] == 'UNDEFINED') {
