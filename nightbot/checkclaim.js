c04aa87d6740597b711234108400c215446f7322
Kenneth Chen <kc5by@virginia.edu>
2022-07-10 01:10:15 -0400

    changed backticks for single quotes

--- nightbot/checkclaim.js
+++ nightbot/checkclaim.js
@@ -1,12 +1,17 @@
+/*
+* INPUT <string> response --- the response to the ClaimTablesGetPokemonClaimLambda
+* OUTPUT <string> --- the nickname associated with the pokemon, or error message if there was one
+*/
+
 if (response['statusCode'] == 500 ) {
-    `Error Getting Data. DM @Kungfu_Kenny98 to see what the error was`
+    'Error Getting Data. DM @Kungfu_Kenny98 to see what the error was'
 }
 else if (response['statusCode'] == 404 ) {
-    `Invalid Pokemon. Please check your spelling. For some special names, check here: https://www.pokeclaim.com/about#About-details`
+    'Invalid Pokemon. Please check your spelling. For some special names, check here: https://www.pokeclaim.com/about#About-details'
 }
 else if (response['body']['discord-id'] == 'UNDEFINED') {
-    `$(1) has not been claimed`;
+    $(1) + ' has not been claimed';
 }
 else {
-    `$(1) was claimed by ${response['body']['discord-username']}: ${response['body']['nickname']}`
+    '$(1) was claimed by ' + response['body']['discord-username'] + ': ' + response['body']['nickname']
 }
